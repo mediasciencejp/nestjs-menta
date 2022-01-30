608aa4384a38aae701d59bccd2b01ab500c8949c
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-01-30 14:19:06 +0100

    add alt controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AltController],
   providers: [AppService],
 })
 export class AppModule {}
